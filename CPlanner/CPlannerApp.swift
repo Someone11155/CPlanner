8d387eba986637b21354b88934ff61d282d52bd7
iLo <someone5551111@gmail.com>
2026-03-26 13:53:30 +0900

    Initial Commit

---
+++ CPlanner/CPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPlannerApp.swift
+//  CPlanner
+//
+//  Created by iLo on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct CPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
